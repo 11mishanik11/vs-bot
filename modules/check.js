82de8248c34f90074bb766e72030e33430d2b8b8
Shabrov <sabrovmisa4@gmail.com>
2023-05-25 23:24:37 +0300

    add check locations

--- modules/check.js
+++ modules/check.js
@@ -1,12 +1,11 @@
 import { getPageHtml } from "./getPageHtml.js";
 import getClanName from './getClanName.js'
 
-// const times = ['02', '05', '10', '15']
+const times = ['02', '05', '10', '15']
 
 export async function check(bot) {
     let data = await getPageHtml()
 
-    /*
     for (let loc in data.one) {
         let thisLoc = data.one[loc]
     
@@ -14,14 +13,14 @@ export async function check(bot) {
             const timeLeft = thisLoc.status.slice(25, 27)
     
             if(times.includes(timeLeft)) {
-                bot.telegram.sendMessage(process.env.CHAT_ID,
+                bot.telegram.sendMessage(-1001913180942,
                 `<b>${thisLoc.name}</b>\n`+
                 `До атаки осталось где-то ${timeLeft} мин`, {
                     parse_mode: 'HTML'
                 })
             }
         } else if(thisLoc.status.includes('Прием заявок')) {
-            bot.telegram.sendMessage(process.env.CHAT_ID,
+            bot.telegram.sendMessage(-1001913180942,
             `<b>${thisLoc.name}</b>\n`+
             `Сбор заявок на атаку - <b>ЖМЯКАМИ</b>`, {
                 parse_mode: 'HTML'
@@ -31,7 +30,7 @@ export async function check(bot) {
     for (let loc in data.two) {
         let thisLoc = data.two[loc]
         if(thisLoc.status == 'mobs') {
-            bot.telegram.sendMessage('168821424',
+            bot.telegram.sendMessage(-1001913180942,
                 `<b>${thisLoc.name}</b>\n`+
                 `Локация занята монстрами`, {
                 parse_mode: 'HTML'
@@ -40,7 +39,7 @@ export async function check(bot) {
             const timeLeft = thisLoc.status.timeleft.slice(25, 27)
     
             if(times.includes(timeLeft) || timeLeft == '14') {
-                bot.telegram.sendMessage(process.env.CHAT_ID,
+                bot.telegram.sendMessage(-1001913180942,
                 `<b>${thisLoc.name}</b>\n`+
                 `До атаки осталось где-то ${timeLeft} мин`, {
                     parse_mode: 'HTML'
@@ -48,7 +47,6 @@ export async function check(bot) {
             }
         }
     }
-    */
 
     for (let castle of data.castles) {
         if(castle.attackInfo && (castle.attackInfo.time.indexOf('04') !== -1)) {
