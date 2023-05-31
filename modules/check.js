be4fa5ee641f9c028d070e1653460add5bdc3111
Shabrov <sabrovmisa4@gmail.com>
2023-05-31 15:21:46 +0300

    попытки сделать сообщ о неуд.атаке, добавил условие +-минута по времени

--- modules/check.js
+++ modules/check.js
@@ -12,6 +12,13 @@ const storeData = {
 }
 const times = ['02', '07', '15']
 
+let timeMs = (str) => {
+  let regex = str.match(/(\d+)\s*ч\s+(\d+)\s*мин/)
+  let hourMs = parseInt(regex[1]) * 3600000
+  let minMs = parseInt(regex[2]) * 60000
+  return hourMs + minMs
+}
+
 async function thisCastle (castle) {
   if (storeData.castles.length < 4) {
     let item = {
@@ -79,12 +86,15 @@ export async function check(bot) {
     //         }
     //     }
     // }
-    console.log('*************************************')
+
+    console.log('************* Логи с проверки *****************')
     for (let castle of data.castles) {
       let castleState = await thisCastle(castle)
 
       switch (castle.attackInfo.attack) {
         case "preAttack":
+          console.log(`Подготовка к атаке: ${castle.attackInfo.time}`)
+
           if (castle.attackInfo.time.indexOf('04') !== -1) {
             castleState.attackInfo.startTime = new Date()
             bot.telegram.sendMessage(process.env.CHAT_ID,
@@ -115,75 +125,41 @@ export async function check(bot) {
           break
         case "noAttack":
           if (castle.attackInfo.time === '00 ч 00 мин') {
-            console.log('Штурм через 4 минуты')
+            console.log('Замок захватили')
+
             castleState.thisClan.name = await getClanName(castle.thisClan)
             castleState.timeMessageAttack = null
+            castleState.attackInfo.startTime = null
+
             bot.telegram.sendMessage(process.env.CHAT_ID,
               `<b>${castle.name}</b>\n`+
               `<b>${await getClanName(castle.thisClan)}</b> успешно захватили замок!`,
               {parse_mode: 'HTML'}
             )
-          } else if (castleState.thisClan.timeAfterAttack && (castleState.thisClan.timeAfterAttack === castle.attackInfo.time)) {
-            console.log('Сообщение о не удачном штурме')
-            castleState.thisClan.timeAfterAttack = null
-            bot.telegram.sendMessage(process.env.CHAT_ID,
-              `<b>${castle.name}</b>\n`+
-              `Штурм закончился не удачно, <b>${await getClanName(castle.thisClan)}</b> остаются в замке`,
-              {parse_mode: 'HTML'}
-            )
           } else {
-            castleState.thisClan.timeAfterAttack = castle.attackInfo.time
-            console.log('Обновление времени в кеше для: ' + castle.name)
+            if (castleState.thisClan.timeAfterAttack && castleState.attackInfo.startTime) {
+              let difference = new Date() - castleState.attackInfo.startTime
+              let lastTime = timeMs(castleState.thisClan.timeAfterAttack)
+              let newTime = timeMs(castle.attackInfo.time)
+
+              console.log('Разница ' + difference)
+              console.log(lastTime + '< Старое & Новое >' + newTime)
+              console.log(lastTime === newTime)
+
+              if ((lastTime + difference) >= (newTime + 60000) || (lastTime + difference) <= (newTime - 60000)) {
+                console.log(`Атака закончилась не удачно, ${castleState.thisClan.name} отбили атаку`)
+              }
+
+              castleState.timeMessageAttack = null
+              castleState.attackInfo.startTime = null
+            } else {
+              castleState.thisClan.timeAfterAttack = castle.attackInfo.time
+              console.log('Обновление времени в кеше для: ' + castle.name)
+            }
           }
           break
       }
-
-      console.log(castleState)
     }
-
-    // Проверка замков
-    /*for (let castle of data.castles) {
-
-      let castleState = await thisCastle(castle)
-
-      if(castle.attackInfo.attack === 'preAttack' && (castle.attackInfo.time.indexOf('04') !== -1)) {
-        bot.telegram.sendMessage(process.env.CHAT_ID,
-          `${castleState.smiley.smiley}<b>${castle.name}</b>\n`+
-          `Текущий клан: ${await getClanName(castle.thisClan)}\n\n`+
-          `Атакующий клан: ${await getClanName(castle.attackInfo.attackClan)}\n`+
-          `⚔️До атаки <b>${castle.attackInfo.time}</b>`,
-          {parse_mode: 'HTML'}
-        )
-      } else if(castle.attackInfo.attack === 'attack') {
-
-        function sendMessage() {
-          console.log('Штурм начался, отправляю сообщение')
-          castleState.timeMessageAttack = new Date()
-          bot.telegram.sendMessage(process.env.CHAT_ID,
-            `<b>${castle.name}</b>\n`+
-            `Штурм начался!`,
-            {parse_mode: 'HTML'}
-          )
-        }
-
-        if (castleState.timeMessageAttack) {
-          if (new Date() - castleState.timeMessageAttack > 900000) {
-            sendMessage()
-          } else console.log('Штурм все еще идет, крайнее сообщение о штурме было меньше 15 минут назад')
-        } else sendMessage()
-      } else if (castle.attackInfo.attack === 'noAttack') {
-        if (castle.attackInfo.time === '00 ч 00 мин') {
-          castleState.timeMessageAttack = null // Очищаем кеш сообщения о штурме
-          bot.telegram.sendMessage(process.env.CHAT_ID,
-            `<b>${castle.name}</b>\n`+
-            `Атака закончилась в пользу: <b>${await getClanName(castle.thisClan)}</b>`,
-            {parse_mode: 'HTML'}
-          )
-        } else if (castle.attackInfo.time === '') {
-        // Уведомление о том, что атака прошла не удачно и клан остается прежним
-        }
-      }
-    }*/
     console.log('*************************************')
   } catch (e) {
     throw e
--- modules/getPageHtml.js
+++ modules/getPageHtml.js
@@ -1,86 +1,90 @@
 import getLocalDom from '../helpes/getLocalDom.js'
 
 export async function getPageHtml() {
-  let $ = await getLocalDom('https://vsmuta.com/info/locs')
+  try {
+    let $ = await getLocalDom('https://vsmuta.com/info/locs')
 
-  let locations = []
+    let locations = []
 
-  // Собираем массив локаций
-  $('.col-12.col-md-6.text-center.mt-2').each(function () {
-    locations.push($(this))
-  })
+    // Собираем массив локаций
+    $('.col-12.col-md-6.text-center.mt-2').each(function () {
+      locations.push($(this))
+    })
 
-  // Собираем замки в массив
-  let castles = []
-  $('.row.site-block-info.mx-0 .col-12.px-0').each(function () {
-    castles.push($(this))
-  })
+    // Собираем замки в массив
+    let castles = []
+    $('.row.site-block-info.mx-0 .col-12.px-0').each(function () {
+      castles.push($(this))
+    })
 
-  // Собираем данные в объект
-  return {
-    // Инфа о отбивах
-    one: (function() {
-      let items = []
+    // Собираем данные в объект
+    return {
+      // Инфа о отбивах
+      one: (function() {
+        let items = []
 
-      for(let i=0; i <= 1; i++) {
-        items.push({
-          name: locations[i].find('.mt-2').text(),
-          nextTime: locations[i].find('.mt-2').next().text(),
-          status: locations[i].find('.mt-3').last().text().trim()
-        })
-      }
-      return items
-    })(),
+        for(let i=0; i <= 1; i++) {
+          items.push({
+            name: locations[i].find('.mt-2').text(),
+            nextTime: locations[i].find('.mt-2').next().text(),
+            status: locations[i].find('.mt-3').last().text().trim()
+          })
+        }
+        return items
+      })(),
 
-    // Данные о нападении мобов
-    two: (function() {
-      let items = []
+      // Данные о нападении мобов
+      two: (function() {
+        let items = []
 
-      for(let i=2; i <= 8; i++) {
-        items.push({
-          name: locations[i].find('.mt-2').text(),
+        for(let i=2; i <= 8; i++) {
+          items.push({
+            name: locations[i].find('.mt-2').text(),
 
-          status: (function() {
-            if(locations[i].find('.mt-3').find('.text-danger.mt-3').text().includes('атаки осталось')) {
-              return {
-                nextTime: locations[i].find('.mt-3').find('div').first().text(),
-                timeLeft: locations[i].find('.mt-3').find('.text-danger.mt-3').text()
-              }
-            } else if(locations[i].find('.mt-3').text().includes('занята монстрами')) {
-              return 'mobs'
-            } else return null
-          })()
-        })
-      }
-      return items
-    })(),
-
-    // Данные о замках
-    castles: (function () {
-      let items = []
-
-      for (let i = 0; i < castles.length; i++ ) {
-        items.push({
-          name: castles[i].find('.d-inline.d-md-none.d-xl-inline').text().trim(),
-          thisClan: castles[i].find('> img').attr('src'),
-          attackInfo: (function () {
-            if (castles[i].find('.float-end > span').text()) {
-              if (castles[i].find('.float-end > span').text() !== 'Идет штурм') {
+            status: (function() {
+              if(locations[i].find('.mt-3').find('.text-danger.mt-3').text().includes('атаки осталось')) {
                 return {
-                  attack: 'preAttack',
-                  time: castles[i].find('.float-end > span').text().trim(),
-                  attackClan: castles[i].find('.float-end > img').attr('src'),
+                  nextTime: locations[i].find('.mt-3').find('div').first().text(),
+                  timeLeft: locations[i].find('.mt-3').find('.text-danger.mt-3').text()
                 }
-              } else return {attack: 'attack'}
-            } else return {
-              attack: 'noAttack',
-              time: castles[i].find('.float-end').text().trim()
-            }
-          })(),
-        })
-      }
-      return items
-    })(),
+              } else if(locations[i].find('.mt-3').text().includes('занята монстрами')) {
+                return 'mobs'
+              } else return null
+            })()
+          })
+        }
+        return items
+      })(),
+
+      // Данные о замках
+      castles: (function () {
+        let items = []
+
+        for (let i = 0; i < castles.length; i++ ) {
+          items.push({
+            name: castles[i].find('.d-inline.d-md-none.d-xl-inline').text().trim(),
+            thisClan: castles[i].find('> img').attr('src'),
+            attackInfo: (function () {
+              if (castles[i].find('.float-end > span').text()) {
+                if (castles[i].find('.float-end > span').text() !== 'Идет штурм') {
+                  return {
+                    attack: 'preAttack',
+                    time: castles[i].find('.float-end > span').text().trim(),
+                    attackClan: castles[i].find('.float-end > img').attr('src'),
+                  }
+                } else return {attack: 'attack'}
+              } else return {
+                attack: 'noAttack',
+                time: castles[i].find('.float-end').text().trim()
+              }
+            })(),
+          })
+        }
+        return items
+      })(),
+    }
+  } catch (e) {
+    throw e
   }
 }
 
